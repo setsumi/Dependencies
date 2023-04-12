8d33657469f711181105316f0fc4987cf6b41819
Arno Klenke <arno_klenke@outlook.com>
2023-04-12 23:27:29 +0200

    Update appsdk

--- DependenciesWAS/MainWindow.xaml.cs
+++ DependenciesWAS/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Dependencies.Helpers;
-using Microsoft.UI;
+﻿using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
 using System;
 using WinRT;
 
@@ -46,25 +46,6 @@ namespace Dependencies
 		private void Window_Closed(object sender, WindowEventArgs args)
 		{
 			(App.Current as App).AppExit();
-
-			// Make sure any Mica/Acrylic controller is disposed so it doesn't try to
-			// use this closed window.
-			if (_micaController != null)
-			{
-				_micaController.Dispose();
-				_micaController = null;
-			}
-		}
-
-		private void Window_Activated(object sender, WindowActivatedEventArgs args)
-		{
-			if(_micaController != null)
-				_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
-		}
-
-		private void RootGrid_ActualThemeChanged(FrameworkElement sender, object args)
-		{
-			UpdateMicaTheme();
 		}
 
 		public static MainWindow GetWindow()
@@ -97,48 +78,14 @@ namespace Dependencies
 		{
 			if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
 			{
+				this.SystemBackdrop = new MicaBackdrop();
 				RootGrid.Background = null;
-
-				_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
-				_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
-
-				// Hooking up the policy object
-				_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
-
-				// Initial configuration state.
-				_configurationSource.IsInputActive = true;
-
-				_micaController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
-
-				UpdateMicaTheme();
-
-				// Enable the system backdrop.
-				// Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
-				_micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
-				_micaController.SetSystemBackdropConfiguration(_configurationSource);
 				return true; // succeeded
 			}
 			return false; // Mica is not supported on this system
 		}
 
-
-		private void UpdateMicaTheme()
-		{
-			if (_micaController != null)
-			{
-				switch (((FrameworkElement)this.Content).ActualTheme)
-				{
-					case ElementTheme.Dark: _configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
-					case ElementTheme.Light: _configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
-					case ElementTheme.Default: _configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
-				}
-			}
-		}
-
 		private static MainWindow _mainWindow;
 		private static IntPtr _windowHandle;
-		WindowsSystemDispatcherQueueHelper _wsdqHelper; // See separate sample below for implementation
-		Microsoft.UI.Composition.SystemBackdrops.MicaController _micaController;
-		Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration _configurationSource;
 	}
 }
